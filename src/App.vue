bdcde46f64888561bbd762a6f6acfa705aed3902
Giwan Lim <nyeok.inae@gmail.com>
2017-08-04 18:49:29 +0900

    sass 작업물과 라우팅 작업물을 합침

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   #app
-  
-
-
+    router-view(name="header")
+    hr
+    router-view
 </template>
 
 <script>
@@ -12,5 +12,18 @@ export default {
 </script>
 
 <style lang="sass">
-  @import "./sass/stylesheet.scss"
+html
+  font-size: 100%
+  background: #fff
+body
+  margin: 0
+    left: 50px
+#app
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  margin-top: 60px
+  text-align: left
+  color: #2c3e50
+  ul
+    text-align: left
 </style>
