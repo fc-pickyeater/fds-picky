56fc9c7f1e322a6681069e8655a1e2cdf2d98170
Giwan Lim <nyeok.inae@gmail.com>
2017-08-22 22:57:25 +0900

    start vuex

--- src/store/store.js
+++ src/store/store.js
@@ -1,30 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import VueRouter from 'vue-router';
+import userData from './user/user_data';
 
 Vue.use(Vuex);
 Vue.use(VueRouter);
 
 export default new Vuex.Store({
   // strict mode
-  state: {
-    isLogined: false,
-    loginedEmail: '',
-    loginedProfileImg: null,
-  },
-  getters: {
-    getIsLogined(state) {
-      return state.isLogined;
-    },
-    getLoginedEmail(state) {
-      return state.loginedEmail;
-    },
-    getLoginedProfileImg(state) {
-      return state.loginedProfileImg;
-    },
-  },
-  mutations: {
-
+  modules: {
+    userData,
   },
 });
 
---
+++ src/store/user/user_data.js
@@ -0,0 +1,22 @@
+const userData = {
+  state: {
+    loginedEmail: '',
+    loginedProfileImg: null,
+  },
+  getters: {
+    getLoginedEmail(state) {
+      return state.loginedEmail;
+    },
+    getLoginedProfileImg(state) {
+      return state.loginedProfileImg;
+    },
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+};
+
+export default userData;
