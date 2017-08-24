221ab87688d2878ae0f124bf411d5cd099c8e6c0
Giwan Lim <nyeok.inae@gmail.com>
2017-08-25 01:39:20 +0900

    solving resolve error 2

---
+++ src/components/recipe/RecipeBook.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+ul(:name="this.bookName" :class="this.bookClass")
+  recipe-item(v-for="(item, index) in this.recipeBook" :index="index" :recipeItem="item" :key="index")
+</template>
+
+<script>
+import RecipeItem from '@/components/recipe/RecipeItem';
+
+export default {
+  name: 'recipe-book',
+  mounted() {
+    this.recipeBook = this.$store.getters.getMyRecipeList.slice();
+    const currentPath = this.$router.currentRoute.path;
+    if (currentPath === '/mypage') {
+      this.bookName = 'my-recipe-book';
+      this.bookClass = 'my-recipe-book';
+    }
+  },
+  components: {
+    RecipeItem,
+  },
+  data() {
+    return {
+      recipeBook: [],
+      bookClass: '',
+      bookName: '',
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
